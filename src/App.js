6527a9cdd6a26cc579767664697774ddbbf79347
a19vikth <viktor.thiberg@hotmail.com>
2022-05-01 13:51:06 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComp from './Navigation/Navbar.js';
-import Math from './pages/Math.js';
+
 import school from './images/school.jpg';
 import './App.css';
 
+import Math from './pages/Math';
+import Swedish from './pages/Swedish';
+import English from './pages/English';
 
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <NavbarComp/>
-      </div>
-      <header className="App-header1">
-
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <div >
-          <img src={school} className="Image-header" alt="school" />
+      <Router>
+        <div>
+          <NavbarComp/>
         </div>
+        <header className="App-header1">
 
-      </header>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <div >
+            <img src={school} className="Image-header" alt="school" />
+          </div>
+       </header>
       
 
       <div>
-        <Math/>
+      
+      <Routes>
+                    <Route path="Swedish" element={<Swedish />}>
+                        
+                    </Route>
+                    <Route path="/English" element={<English />}>
+                        
+                    </Route>
+                    <Route path="/Math" element={<Math />}>
+                        
+                    </Route>
+                </Routes>
+                
       </div>
+      </Router>
     </div>
     
     
--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -9,50 +9,29 @@ import {
     Link
 } from "react-router-dom";
 
-import Math from '../pages/Math';
-import Swedish from '../pages/Swedish';
-import English from '../pages/English';
+
 
 export default class NavbarComp extends Component {
     render() {
         return (
-            <Router>
-            <div>
+            
+            <Navbar bg="dark" variant={"dark"} expand="lg">
+                <Navbar.Brand href="#">School-App</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="mr-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link as={Link} to="/Math">Math</Nav.Link>
+                        <Nav.Link as={Link} to="/English">English</Nav.Link>
+                        <Nav.Link as={Link} to="/Swedish">Swedish</Nav.Link>
 
-                <Navbar bg="dark" variant={"dark"} expand="lg">
-                    <Navbar.Brand href="#">School-App</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
-                        <Nav
-                            className="mr-auto my-2 my-lg-0"
-                            style={{ maxHeight: '100px' }}
-                            navbarScroll
-                        >
-                            <Nav.Link as={Link} to="/Math">Math</Nav.Link>
-                            <Nav.Link as={Link} to="/English">English</Nav.Link>
-                            <Nav.Link as={Link} to="/Swedish">Swedish</Nav.Link>
+                    </Nav>
 
-                        </Nav>
-
-                    </Navbar.Collapse>
-                </Navbar>
-            </div>
-            <div>
-                
-                <Routes>
-                    <Route path="Swedish" element={<Swedish />}>
-                        
-                    </Route>
-                    <Route path="/English" element={<English />}>
-                        
-                    </Route>
-                    <Route path="/Math" element={<Math />}>
-                        
-                    </Route>
-                </Routes>
-          
-            </div>
-        </Router>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 }
